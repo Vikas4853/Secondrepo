1a03a234859b842d2354e20251475ba196709279
DELL <DELL@10.10.10.87>
2021-03-03 23:56:04 +0530

    Frist Commit  By vika

---
+++ Seconddemo/src/main/java/com/practise/Method_One.java
@@ -0,0 +1,14 @@
+package com.practise;
+
+public class Method_One {
+
+	public static  void m1() {
+		
+		System.out.println("frist methos print ");
+		
+		
+	}
+	
+	
+	
+}
---
+++ Seconddemo/src/main/java/com/practise/Second_Method.java
@@ -0,0 +1,27 @@
+package com.practise;
+
+public class Second_Method {
+
+	public void m2() {
+		
+		System.out.println("Print System Two method"); 
+	}
+	
+	
+	public static void main(String [] arf ) {
+		
+		Second_Method sc = new Second_Method();
+		
+		sc.m2();
+		
+		 Method_One.m1();
+		 
+		System.out.println("Print main method 1");
+		
+		
+		System.out.println("main method print 2");
+		
+		
+	}
+	
+}
